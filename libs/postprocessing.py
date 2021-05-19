d03d76042c980bd3b83581015c2dc999e5489630
Aquib <aquibalicool@yahoo.in>
2021-05-19 19:55:54 +0530

    adding comments

--- libs/postprocessing.py
+++ libs/postprocessing.py
@@ -1,25 +1,3 @@
-"""
-Name: Post-processing class file
-Description: This file contains post-processing classes.
-Version: [release][3.2]
-Source url: https://github.com/OPHoperHPO/image-background-remove-tool
-Author: Anodev (OPHoperHPO)[https://github.com/OPHoperHPO] .
-License: Apache License 2.0
-License:
-   Copyright 2020 OPHoperHPO
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
 import logging
 
 from PIL import Image
